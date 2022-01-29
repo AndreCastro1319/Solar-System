09654f9ab0f7fdb1785e4c12e2e70039950cca7c
jimmycastro <ndrecastro@gmail.com>
2022-01-29 10:55:05 -0500

    Solar System Specifications added, somefeatures still under development

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        //write your code here
+        Main milkyWay = new Main();
+
+    }
+}
---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class SolarSystem {
+        String planet;
+        String color;
+}
