09e4de2258f2572fcf34b865e602985f5aa0d4a4
jimmycastro <ndrecastro@gmail.com>
2022-01-29 11:05:30 -0500

    The basic structure is complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         //write your code here
-        Main milkyWay = new Main();
+        SolarSystem milkyWay = new SolarSystem();
+            milkyWay.moon="1";
+            milkyWay.sun="1";
+            milkyWay.planets="12";
 
     }
 }
--- src/com/company/SolarSystem.java
+++ src/com/company/SolarSystem.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class SolarSystem {
-        String planet;
-        String color;
+        String sun;
+        String moon;
+        String planets;
 }
